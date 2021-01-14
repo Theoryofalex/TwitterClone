37677da6fb93b0232a997df95a2fd472ad2d88f0
Alex Gomez <alexgomez.cosmic@gmail.com>
2021-01-13 23:14:10 -0800

    Added a component. Tweets now visiable

---
+++ src/components/Tweet.vue
@@ -0,0 +1,11 @@
+<template>
+    <article class="tweet border-b bborder-gray-600">
+        <div class="text-white">{{ tweet.author.display_name }}</div>
+    </article>
+</template>
+
+<script>
+export default {
+    props: ['tweet']
+}
+</script>
